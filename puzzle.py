a3a73feb7fcdfdf4e3ace2570d864665d31c7358
Abena <priscillaabenaaboagye@gmail.com>
2025-02-13 12:12:04 +0000

    defining the function

--- puzzle.py
+++ puzzle.py
@@ -8,6 +8,18 @@ rods = {
 }
 
 def move(n, source, auxiliary, target):
+    # display starting configuration
+    for i in range(number_of_moves):
+        remainder = (i + 1) % 3
+        if remainder == 1:
+            print(f"Move {i + 1} allowed between {source} and {target}")
+            forward = False
+            if not rods[target]:
+                forward = True
+        elif remainder == 2:
+            print(f"Move {i + 1} allowed between {source} and {auxiliary}")
+        elif remainder == 0:
+            print(f"'Move {i + 1} allowed between {auxiliary} and {target}'")
     print(rods)
 
 # initiate call from source A to target C with auxiliary B
