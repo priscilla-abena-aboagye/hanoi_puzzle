c1aed6b13da2733120db01dc8f4dcd2602b24d23
Abena <priscillaabenaaboagye@gmail.com>
2025-03-08 10:39:22 +0000

    Update duplicte.py

--- duplicte.py
+++ duplicte.py
@@ -1,22 +1,21 @@
-number_of_disks = 3
+number_of_disks = 5
 
-rods = {
-    "A": list(range(number_of_disks, 0, -1)),
-    "B": [],
-    "C": []
-}
-
+A = list(range(number_of_disks, 0, -1))
+B = []
+C = []
 
 def move(n, source, auxiliary, target):
     if n > 0:
     # move n - 1 disks from source to auxiliary, so they are out of the way
         move(n-1, source, target, auxiliary)
+        
         # move the nth disk from source to target
-        rods[target].append(rods[source].pop())
+        target.append(source.pop())
 
         # display our progress
-        print(rods, "\n")
+        print(A,B,C, "\n")
 
+        # move the n - 1 disks that we left on auxiliary onto target
         move(n-1, auxiliary, source, target)
 
     
